c6282cd550a53981212633869b58a81aa7eb6756
Rampugnant <ncfeemster@gmail.com>
2023-01-26 11:38:13 -0900

    added correct formatting

--- birdClass.h
+++ birdClass.h
@@ -3,16 +3,16 @@
 
 class Bird {
     private:
-    std::string name;
-    double wingspan;
-    std::string beakShape;
-    bool fried;
+        std::string name;
+        double wingspan;
+        std::string beakShape;
+        bool fried;
 
     public:
 
-    Bird();
-    Bird(std::string name, double wingspan, std:string beakShape, bool fried);
+        Bird();
+        Bird(std::string name, double wingspan, std:string beakShape, bool fried);
 
-    void fry();
-    bool isFried();
+        void fry();
+        bool isFried();
 };
