839f4bb5c623f33aedb2758033b0c6e977ac43eb
Alexander <aejohnson13@alaska.edu>
2023-01-26 19:34:11 -0900

    created birdClass.cpp

---
+++ birdClass.h
@@ -0,0 +1,25 @@
+#include "birdClass.h" 
+
+Bird::Bird() 
+{
+	name;
+	wingspan;
+	beakShape;
+	fried; 
+}
+
+Bird::Bird(std::string name, double wingspan, std::string beakShape, bool fried)
+{ 
+	name = name; 
+	wingspan = wingspan; 
+	beakShape = beakShape; 
+	fried = fried; 
+} 
+
+void Bird::fry(){ 
+	//updates boolian fried to true 
+} 
+
+bool Bird::isFried(){ 
+	//returns the bool fried within a bird object 
+}  
